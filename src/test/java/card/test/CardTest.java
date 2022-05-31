d54b6fe4b8a7ca7e5cbc1e7548212e3ee6275781
Alexander211996 <rubolov.alexander@mail.ru>
2022-05-31 11:41:21 +0300

    All tests

--- src/test/java/card/test/CardTest.java
+++ src/test/java/card/test/CardTest.java
@@ -47,6 +47,19 @@ public class CardTest {
     }
 
     @Test
+    public void shouldSendFormWithDoubleName () {
+        driver.get ("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Анна-Мария Петрова");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79991112233");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id = 'order-success']")).getText().trim();
+        assertEquals (expectedText, actualText);
+    }
+
+    @Test
     public void shouldNotSendFormWithoutCheckbox () {
         driver.get ("http://localhost:9999/");
         driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Иван Петров");
@@ -59,7 +72,7 @@ public class CardTest {
     }
 
     @Test
-    public void shouldnotSendFormWithEnglish () {
+    public void shouldNotSendFormWithEnglish () {
         driver.get ("http://localhost:9999/");
         driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Ivan");
         driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79991112233");
@@ -68,6 +81,71 @@ public class CardTest {
 
         String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         String actualText = driver.findElement(By.cssSelector("span.input__sub")).getText().trim();
+        assertEquals (expectedText, actualText);
+    }
+
+    @Test
+    public void shouldNotSendFormWithNumbers () {
+        driver.get ("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("123");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79991112233");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actualText = driver.findElement(By.cssSelector("span.input__sub")).getText().trim();
+        assertEquals (expectedText, actualText);
+    }
+
+    @Test
+    public void shouldNotSendFormWithSymbols () {
+        driver.get ("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("@#$");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79991112233");
+        driver.findElement(By.cssSelector("div:nth-child(3) > label")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actualText = driver.findElement(By.cssSelector("span.input__sub")).getText().trim();
+        assertEquals (expectedText, actualText);
+    }
+
+    @Test
+    public void shouldNotSendFormWithLessThan11Tel () {
+        driver.get ("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+7999111223");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actualText = driver.findElement(By.cssSelector("div:nth-child(2) > span > span > span.input__sub")).getText().trim();
+        assertEquals (expectedText, actualText);
+    }
+
+    @Test
+    public void shouldNotSendFormWithMoreThan11Tel () {
+        driver.get ("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+799911122334");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actualText = driver.findElement(By.cssSelector("div:nth-child(2) > span > span > span.input__sub")).getText().trim();
+        assertEquals (expectedText, actualText);
+    }
+
+    @Test
+    public void shouldNotSendFormWithoutPlus () {
+        driver.get ("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("79991112233");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actualText = driver.findElement(By.cssSelector("div:nth-child(2) > span > span > span.input__sub")).getText().trim();
         assertEquals (expectedText, actualText);
     }
 
