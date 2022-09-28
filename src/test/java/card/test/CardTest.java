c1d657c976f8966e5e1fa50ceee9f1000a9c02d1
Alexander211996 <91474414+Alexander211996@users.noreply.github.com>
2022-09-28 15:52:12 +0300

    Update CardTest.java

--- src/test/java/card/test/CardTest.java
+++ src/test/java/card/test/CardTest.java
@@ -23,8 +23,16 @@ public class CardTest {
     }
 
     @BeforeEach
-    void setup() {
-        driver = new ChromeDriver();
+       void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
